b9783f37919be9d95cac9e24f7110f736f46092d
Alexander <stallev@yahoo.com>
2020-09-15 18:35:41 +0300

    CORRECTED PHP

--- mail-class.php
+++ mail-class.php
@@ -1,7 +1,6 @@
 <?php
   if (isset($_POST["phone"]) && isset($_POST["flag"])) { 
 
-    // Формируем массив для JSON ответа
       $result = array(
         'phone' => $_POST["phone"],
         'flag' => $_POST["flag"]
